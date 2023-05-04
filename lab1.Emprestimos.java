150dd129bb36d29cb65b4138e8a37e2a50f380bf
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2023-05-04 15:21:24 -0300

    Create lab1.Emprestimos.java

---
+++ lab1.Emprestimos.java
@@ -0,0 +1,13 @@
+package labpratorio1;
+
+import java.util.ArrayList;
+
+public class Emprestimos {
+	private ArrayList<Publicações> listap = new ArrayList<>();
+	
+	public Emprestimos(ArrayList<Publicações> lista) {
+		this.listap=lista;
+	}
+
+	
+}
