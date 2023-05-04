48a989c96f2b3c449584865987b43eb880752aed
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2023-05-04 15:21:57 -0300

    Create lab1.livro.java

---
+++ lab1.livro.java
@@ -0,0 +1,10 @@
+package labpratorio1;
+
+public class livro extends Publicações {
+
+	public livro(String titulo, int ano, String autor, int quant) {
+		super(titulo, ano, autor, quant);
+		
+	}
+
+}
